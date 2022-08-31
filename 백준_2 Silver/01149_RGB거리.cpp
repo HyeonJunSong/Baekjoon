1703b735c6f4b39680d18347aeea32421d5523c0
HyeonJunSong <music9801@naver.com>
2022-08-31 18:58:10 +0900

    1149

--- 백준_2 Silver/01149_RGB거리.cpp
+++ 백준_2 Silver/01149_RGB거리.cpp
@@ -1,98 +1,39 @@
+////2022.08.31
+//이해 및 설계 : 11m 25s
+//입력 : 1m 32s
+//구현 : 12m 28s
+
+//tot : 25m 47s
+
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
-vector<int> R;
-vector<int> G;
-vector<int> B;
-
-int** memo;
-int N;
-
-int minCost(int curind, int banColor);
-
+int minBetTwo(int a, int b);
 
 int main(){
+    int N;
     cin >> N;
-
-    for(int i = 0; i < N; i++){
-        int r,g,b;
-        cin >> r >> g >> b;
-        R.push_back(r);
-        G.push_back(g);
-        B.push_back(b);
-    }
-
-    memo = new int*[3];
-    for(int i = 0; i < 3; i++){
-        memo[i] = new int[N];
-        for(int j = 0; j < N; j++)
-            memo[i][j] = 0;
-    }
-
-    int minR = minCost(0, 0);
-    int minG = minCost(0, 1);
-    int minB = minCost(0, 2);
-
-    int min = minR;
-    if(min > minG)
-        min = minG;
-    if(min > minB)
-        min = minB;
+    vector<vector<int>> cost(N, vector<int>(3));
+    for(int i = 0; i < N; i++)
+        cin >> cost[i][0] >> cost[i][1] >> cost[i][2];
     
-    cout << min;
+    vector<int> memo = cost[0];
+
+    vector<int> temp;
+    for(int i = 1; i < N; i++){
+        temp = memo;
+        memo[0] = cost[i][0] + minBetTwo(temp[1], temp[2]);
+        memo[1] = cost[i][1] + minBetTwo(temp[0], temp[2]);
+        memo[2] = cost[i][2] + minBetTwo(temp[1], temp[0]);
+    }
+
+    cout << minBetTwo(memo[0], minBetTwo(memo[1], memo[2]));
 
     return 0;
 }
 
-int minCost(int curind, int banColor){
-    if(!memo[banColor][curind])
-        return memo[banColor][curind];
-
-    int min = 0;
-
-    if(curind == N - 1){
-        switch(banColor){
-            case 0:
-                G[N - 1] < B[N - 1] ? min = G[N - 1]: min = B[N - 1];
-                break;
-
-            case 1:
-                R[N - 1] < B[N - 1] ? min = R[N - 1]: min = B[N - 1];
-                break;
-
-            case 2:
-                R[N - 1] < G[N - 1] ? min = R[N - 1]: min = G[N - 1];
-                break;
-        }
-    }
-    else{
-        switch(banColor){
-            case 0:
-                if(G[curind] < B[curind])
-                    min = G[curind] + minCost(curind + 1, 1);
-                else
-                    min = B[curind] + minCost(curind + 1, 2);
-
-                break;
-
-            case 1:
-                if(R[curind] < B[curind])
-                    min = R[curind] + minCost(curind + 1, 0);
-                else
-                    min = B[curind] + minCost(curind + 1, 2);
-                break;
-
-            case 2:
-                if(R[curind] < G[curind])
-                    min = R[curind] + minCost(curind + 1, 0);
-                else
-                    min = G[curind] + minCost(curind + 1, 1);
-                break;
-        }
-    }
-
-    memo[banColor][curind] = min;
-    return min;
+int minBetTwo(int a, int b){
+    return a < b ? a : b;
 }
