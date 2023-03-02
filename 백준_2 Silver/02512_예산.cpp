b7206f41a42dded32bc9c14f5e646974b7d61b3e
HyeonJunSong <music9801@naver.com>
2023-03-02 13:50:01 +0900

    2512

--- 백준_2 Silver/02512_예산.cpp
+++ 백준_2 Silver/02512_예산.cpp
@@ -1,41 +1,86 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <cmath>
 
 using namespace std;
 
+int M, N;
+vector<int> budgets;    //예산을 저장하는 배열
+vector<int> budgetSum;  //예산누적 값을 저장하는 배열
+
+bool ifCanBeCeiling(int n);
+
 int main(){
-    int N;
     cin >> N;
-    vector<int> budgets(N);
+    budgets = vector<int>(N);
+    budgetSum = vector<int>(N);
     for(int i = 0; i < N; i++)
         cin >> budgets[i];
     sort(budgets.begin(), budgets.end());
-    int M;
+    budgetSum[0] = budgets[0];
+    for(int i = 1; i < N; i++)
+        budgetSum[i] = budgetSum[i-1] + budgets[i];
     cin >> M;
 
-    //총 예산 구하기
-    int totBudget = 0;
-    for(int i = 0; i < N; i++)
-        totBudget += budgets[i];
-    
-    if(totBudget <= M){
-        cout << budgets.back();
+
+    if(budgetSum[N - 1] <= M){
+        cout << budgets[N - 1];
         return 0;
     }
 
-    //맨 뒤에서부터 상한선 정하면서 적정 상한선 구하기
-    int curOver = totBudget - M;
-    int accrue = 0;
-    for(int i = (int)(budgets.size()) - 2; i >= 0; i--){
-        accrue += budgets[i + 1] - budgets[i];
-        if(curOver - accrue <= 0){
-            cout << (int)((double)budgets[i + 1] - ((double)(curOver) / ((budgets.size()) - i - 1)));
-            break;
+    int st = 1, md, ed = budgets[N - 1];
+    while(st <= ed){
+        md = (ed + st) / 2;
+        if(ifCanBeCeiling(md)){
+            st = md + 1;
         }
-        curOver -= accrue;
+        else{
+            ed = md - 1;
+        }
     }
 
+    if(ifCanBeCeiling(st))
+        cout << st;
+    else if(ifCanBeCeiling(ed))
+        cout << ed;
+    else
+        cout << ed - 1;
+
     return 0;
+}
+
+bool ifCanBeCeiling(int n){
+    //n보다 크지 않은 최대 인덱스 찾기
+    int st = 0, md, ed = N - 1;
+    while(st <= ed){
+        md = (st + ed) / 2;
+        if(n < budgets[md]){
+            ed = md - 1;
+        }
+        else if(n > budgets[md]){
+            st = md + 1;
+        }
+        else{
+            ed = md;
+            break;
+        }
+    }
+
+    int ind;
+    if(n > budgets[st])
+        ind = st;
+    else if(st == 0)
+        ind = -1;
+    else if(n > budgets[ed])
+        ind = ed;
+    else
+        ind = ed - 1;
+
+    //해당 인덱스가 상한선이 돼도 되는지 리턴
+    if(ind == -1){
+        return n * N <= M;
+    }
+    else{
+        return budgetSum[ind] + (N - 1 - ind) * n <= M;
+    }
 }
