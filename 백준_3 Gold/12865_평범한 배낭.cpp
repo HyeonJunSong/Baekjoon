3b4f22b64b8ebc35e0df9714d633a11b46d7a615
HyeonJunSong <music9801@naver.com>
2023-05-10 15:12:05 +0900

    230510 42040KB 80ms

--- 백준_3 Gold/12865_평범한 배낭.cpp
+++ 백준_3 Gold/12865_평범한 배낭.cpp
@@ -1,77 +1,49 @@
-//22.02.09
-
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
+int N, K;
+vector<pair<int, int>> items;
+vector<vector<int>> cache;
 
-typedef struct Node {
-	int W;
-	int V;
-	bool check;
-}node;
+int getLargestValue(int ind, int spaceLeft);
 
-bool comp(node n1, node n2) {
-	return n1.W > n2.W;
+int main(){
+    cin >> N >> K;
+    items = vector<pair<int, int>>(N);
+    for(int i = 0; i < N; i++)
+        cin >> items[i].first >> items[i].second;
+    
+    cache = vector<vector<int>>(N + 1, vector<int>(K + 1, -1));
+
+    cout << getLargestValue(0, K);
+
+    return 0;
 }
 
-int** memo;
-vector<node> thing;
-int N;
+int getLargestValue(int ind, int spaceLeft){
+    
+    ////기저
+    if(ind == N || spaceLeft <= 0)
+        return 0;
 
-int MaxVal(int K, int prvInd);
+    ////재귀
 
-int main() {
-	int K;
-	cin >> N >> K;
+    //안넣었을 때
+    if(cache[ind + 1][spaceLeft] == -1)
+        cache[ind + 1][spaceLeft] = getLargestValue(ind + 1, spaceLeft);
+    //넣었을 때
+    if(spaceLeft - items[ind].first >= 0){
+        if(cache[ind + 1][spaceLeft - items[ind].first] == -1)
+            cache[ind + 1][spaceLeft - items[ind].first] = getLargestValue(ind + 1, spaceLeft - items[ind].first);
+    }
+    else
+        return cache[ind + 1][spaceLeft];
 
-	memo = new int* [K + 1];
-	for (int i = 0; i < K + 1; i++) {
-		memo[i] = new int[K + 1];
-		for (int j = 0; j < K + 1; j++)
-			memo[i][j] = -1;
-	}
-	for (int i = 0; i < N; i++) {
-		int inpW, inpV;
-		cin >> inpW >> inpV;
-		thing.push_back({ inpW, inpV, false });
-	}
-
-	sort(thing.begin(), thing.end(), comp);
-
-	int ans = MaxVal(K, 0);
-	if (ans < 0) cout << 0;
-	else cout << ans;
-
-
-	return 0;
-}
-
-int MaxVal(int K, int prvInd) {	//K의 무게로 낼 수 있는 최대 V
-	int i = prvInd;	
-	for (; i < N; i++)
-		if (thing[i].W <= K) break;
-	
-
-
-	if (i == N)
-		return 0;
-
-	int Max = -1;
-	for (; i < N; i++) {
-		int cur = memo[K - thing[i].W][prvInd];
-
-		if (cur == -1){
-			int cur = thing[i].V + MaxVal(K - thing[i].W, i + 1);
-			memo[K - thing[i].W][prvInd] = cur;
-		}
-
-		if (Max < cur) {
-			Max = cur;
-		}
-	}
-
-	return Max;
+    ////반환
+    if(cache[ind + 1][spaceLeft] > cache[ind + 1][spaceLeft - items[ind].first] + items[ind].second)
+        return cache[ind + 1][spaceLeft];
+    else
+        return cache[ind + 1][spaceLeft - items[ind].first] + items[ind].second;
 }
