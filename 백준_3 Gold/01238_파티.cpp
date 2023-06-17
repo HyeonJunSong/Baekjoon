7d58b7716b79c63e630a82a2ca2f44cddb8a9bf4
HyeonJunSong <music9801@naver.com>
2023-06-17 14:38:44 +0900

    230617 2468KB 4ms

--- 백준_3 Gold/01238_파티.cpp
+++ 백준_3 Gold/01238_파티.cpp
@@ -1,68 +1,84 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
-vector<vector<pair<int, int>>> graph;
-vector<vector<int>> cache;
+class node{
+public:
+    int st;
+    int ed;
+    int T;
 
-int shortestPath(int st, int ed);
-
-vector<bool> visited;
-
-#define INF 987654321
+    bool operator<(node b) const{
+        return T > b.T;
+    }
+};
 
 int main(){
     int N, M, X;
     cin >> N >> M >> X;
-    graph = vector<vector<pair<int, int>>>(N + 1);
 
-    int st, ed, t;
+    //입력
+    vector<vector<pair<int,int>>> graph(N + 1);
+    vector<vector<pair<int,int>>> graphReverse(N + 1);
     for(int i = 0; i < M; i++){
-        cin >> st >> ed >> t;
-        graph[st].push_back(pair<int, int>(ed, t));
+        int st, ed, T;
+        cin >> st >> ed >> T;
+        graph[st].push_back({ed, T});
+        graphReverse[ed].push_back({st, T});
     }
 
-    cache = vector<vector<int>>(N + 1, vector<int>(N + 1, -1));
+    ////두 다익스트라 공통으로 사용할 변수
+    priority_queue<node> PQ;
+    vector<bool> visited(N + 1, false);
+
+    //다익스트라 _ X부터 최단경로
+    vector<int> shortestFromX(N + 1);
+    PQ.push({X, X, 0});
+    
+    while(!PQ.empty()){
+        node curNode = PQ.top();
+        PQ.pop();
+        
+        if(visited[curNode.ed])
+            continue;
+        visited[curNode.ed] = true;
+        
+        shortestFromX[curNode.ed] = curNode.T;
+
+        for(auto iter = graph[curNode.ed].begin(); iter != graph[curNode.ed].end(); iter++)
+            if(!visited[iter->first])
+                PQ.push({curNode.ed, iter->first, curNode.T + iter->second});
+    }
+
+    //다익스트라 _ X까지 최단경로
+    vector<int> shortestToX(N + 1);
     visited = vector<bool>(N + 1, false);
+    PQ.push({X, X, 0});
+    
+    while(!PQ.empty()){
+        node curNode = PQ.top();
+        PQ.pop();
+        
+        if(visited[curNode.ed])
+            continue;
+        visited[curNode.ed] = true;
+        
+        shortestToX[curNode.ed] = curNode.T;
 
-    int curPath;
-    int ans = 0;
-
-    for(int i = 1; i <= N; i++){
-        if(i == X) continue;
-        if(cache[i][X] == -1)
-            cache[i][X] = shortestPath(i, X);
-        curPath = cache[i][X];
-
-        if(cache[X][i] == -1)
-            cache[X][i] = shortestPath(X, i);
-        curPath += cache[X][i];
-        if(curPath > ans)
-            ans = curPath;
+        for(auto iter = graphReverse[curNode.ed].begin(); iter != graphReverse[curNode.ed].end(); iter++)
+            if(!visited[iter->first])
+                PQ.push({curNode.ed, iter->first, curNode.T + iter->second});
     }
 
-    cout << ans;
 
+    //답구하기
+    int longestPath = 0;
+    for(int i = 1; i < N + 1; i++)
+        if(shortestFromX[i] + shortestToX[i] > longestPath)
+            longestPath = shortestFromX[i] + shortestToX[i];
+    
+    cout << longestPath;
     return 0;
-}
-
-int shortestPath(int st, int ed){
-    visited[st] = true;
-    int shortest = INF;
-    for(auto iter = graph[st].begin(); iter != graph[st].end(); iter++){
-        if(visited[iter->first]) continue;
-        if(iter->first == ed){
-            if(shortest > iter->second)
-                shortest = iter->second;
-        }
-        else{
-            if(cache[iter->first][ed] == -1)
-                cache[iter->first][ed] = shortestPath(iter->first, ed);
-            if(shortest > cache[iter->first][ed] + iter->second)
-                shortest = cache[iter->first][ed] + iter->second;
-        }
-    }
-    visited[st] = false;
-    return shortest;
 }
