40608a937299a7cd86fd5968016919e69a93bed8
HyeonJunSong <music9801@naver.com>
2023-11-20 16:15:40 +0900

    231120 33240KB 452ms

--- 백준_4 Platinum/01413_박스 안의 열쇠.py
+++ 백준_4 Platinum/01413_박스 안의 열쇠.py
@@ -6,8 +6,6 @@ fact = [1]
 for i in range(1, N + 1):
   fact.append(fact[-1] * i)
 
-print(fact)
-
 def groupNM(n, m, maxVal):
   
   if n == 0:
@@ -31,9 +29,27 @@ def groupNM(n, m, maxVal):
       
   return res
 
-groupRes = groupNM(N, M)
+def nCr(n, r):
+  if r == 0 or n == r:
+    return 1
+  return nCr(n - 1, r) + nCr(n - 1, r - 1)
 
+groupRes = groupNM(N, M, N)
+
+totCase = 0
 for e in groupRes:
   curNum = 1
-  for i in range(N + 1):
-    
+  curN = N
+  for i in range(1, N + 1):
+    if e[i] == 0:
+      continue
+    for j in range(e[i]):
+      curNum *= nCr(curN, i)
+      curNum *= fact[i - 1]
+      curN -= i
+    curNum //= fact[e[i]]
+  totCase += curNum
+
+allCase = fact[N]
+
+print(totCase // math.gcd(totCase, allCase), '/', allCase // math.gcd(totCase, allCase), sep="")
