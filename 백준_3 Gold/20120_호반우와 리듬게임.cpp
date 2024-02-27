0b1b16bb48b5f1afae77c4b100f27756074f85ad
HyeonJunSong <music9801@naver.com>
2024-02-28 00:13:20 +0900

    240228 9880KB 24ms

--- 백준_3 Gold/20120_호반우와 리듬게임.cpp
+++ 백준_3 Gold/20120_호반우와 리듬게임.cpp
@@ -1,77 +1,47 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-typedef struct Node {
-	int sc;
-	int combo;
-}node;
+#define LL long long
+#define INF 5005000001
 
+int N;
+vector<LL> notes;
+vector<vector<LL>> memo;
+
+LL getAns(LL idx, LL curCombo){
+
+	if(idx >= N) return 0;
+
+	if(memo[idx][curCombo] != INF)
+		return memo[idx][curCombo];
+
+	int after = max(max(getAns(idx + 1, curCombo + 1), getAns(idx + 2, 1)), getAns(idx + 3, 1));
+
+	memo[idx][curCombo] = notes[idx] * curCombo + after;
+
+	return memo[idx][curCombo];
+}
 
 int main() {
 
-	int N;
 	cin >> N;
-	int* notes = new int[N];
+	notes = vector<LL>(N);
 
 	for (int i = 0; i < N; i++) cin >> notes[i];
+	memo = vector<vector<LL>>(N + 1, vector<LL>(N + 1, INF));
 
-	node* score = new node[N];
+	LL ans = 0;
+	if(ans < getAns(0, 1))
+		ans = getAns(0, 1);
+	if(ans < getAns(1, 1))
+		ans = getAns(1, 1);
+	if(ans < getAns(2, 1))
+		ans = getAns(2, 1);
+	
 
-	//첫번째 노드
-	score[0].sc = notes[0];
-	score[0].combo = 1;
-
-	//두번째 노드
-	if (N >= 2) {
-		score[1].combo = score[0].combo + 1;
-		score[1].sc = score[0].sc + score[1].combo * notes[1];
-	}
-
-	//세번째 노드
-	if (N >= 3) {
-		score[2].combo = score[1].combo + 1;
-		score[2].sc = score[1].sc + score[2].combo * notes[2];
-
-		int secScore = score[0].sc + notes[2];
-		if (secScore > score[2].sc) {
-			score[2].combo = 1;
-			score[2].sc = secScore;
-		}
-
-	}
-
-	//네번째 이상
-	if (N >= 4) {
-		int i = 3;
-
-		while (i < N) {
-			score[i].combo = score[i - 1].combo + 1;
-			score[i].sc = score[i - 1].sc + score[i].combo * notes[i];
-
-			int secScore = score[i - 2].sc + notes[i];
-			if (secScore > score[i].sc) {
-				score[i].combo = 1;
-				score[i].sc = secScore;
-			}
-
-			int trdScore = score[i - 3].sc + notes[i];
-			if (trdScore > score[i].sc) {
-				score[i].combo = 1;
-				score[i].sc = trdScore;
-			}
-
-
-			i++;
-		}
-	}
-
-	if (score[N - 1].sc < 0) {
-		cout << 0;
-	}
-	else {
-		cout << score[N - 1].sc;
-	}
+	cout << ans;
 
 	return 0;
 }
