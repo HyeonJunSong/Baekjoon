68268036fe4ead9b70aa83547610291575db0cfc
HyeonJunSong <music9801@naver.com>
2024-06-11 18:11:07 +0900

    save

---
+++ 백준_3 Gold/29726_숏코딩의 왕 브실이.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main(){
+
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int N, M;
+  cin >> N >> M;
+  vector<int> A(N);
+  for(int i = 0; i < N; i++)
+    cin >> A[i];
+  
+  priority_queue<int> PQ;
+
+
+  for(int i = 1; i < N; i++){
+    
+  }
+
+  return 0;
+}
